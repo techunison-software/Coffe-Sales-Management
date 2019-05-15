2c7b5c636dc4a4313f114d7f10b189ef9ec297f9
Narendran <Narendran@techunison.co.in>
2019-05-15 16:01:47 +0530

    IP-77

--- login.py
+++ login.py
@@ -2,66 +2,126 @@ import pandas as pd
 import sqlalchemy as sql
 import SQLAlchemyCon as conn
 
+import UserModule as userMod
+import PurchaseModule as purchaseMod
+import SalesModule as salesMod
+"""import InventoryModule as inventoryMod   """
+
 sql_engine=conn.getConnection()
 
 class Login:    
 
     def initialCall(self):
         self.user_name=input("Enter User Name :")
-        self.password=input("Enter password : ")
+        self.password=input("Enter password  :")
         self.authenticate()
 
     def authenticate(self):
-        user=self.getUserDetails()   
-        if self.user_name == user["user_name"][0] and self.password == user["password"][0]:
-            print("Login success!")
-            self.user=user            
-            self.showMainMenu()
+        user=self.getUserDetails()
+        if not user.empty:
+            if self.user_name == user["user_name"][0] and self.password == user["password"][0]:
+                print("\tSuccessfully LogedIn!\n")
+                self.user=user            
+                self.showMainMenu()
+            else:
+                print("\tLogin Failed!\n\tInvalid User name or password.")
+                self.user=None
+                self.initialCall()
         else:
-            print("\tLogin Failed!\n\tInvalid User name or password.")
-            self.user=None
+            print("\tLogin Failed!\n\tUser Does not Exist.Please enter a valid user.\n")
             self.initialCall()
 
     def getUserDetails(self):
-        query = 'select u.user_id, u.user_name, u.name, u.email_id, u.password, u.department_id, d.department_name, u.role_id, rt.role, u.status_id, u.created_by, u.created_on, u.updated_by, u.updated_on from users u join role_type rt on rt.role_id=u.role_id join department d on d.department_id=u.department_id where u.user_name=''"'+self.user_name+'"'
-        return pd.read_sql_query(query, sql_engine)        
+        result=None
+        try:
+            query = 'select u.user_id, u.user_name, u.name, u.email_id, u.password, u.department_id, d.department_name, u.role_id, rt.role, u.status_id, u.created_by, u.created_on, u.updated_by, u.updated_on from users u join role_type rt on rt.role_id=u.role_id join department d on d.department_id=u.department_id where u.user_name=''"'+self.user_name+'"'
+            result=pd.read_sql_query(query, sql_engine) 
+        except :
+            print('An error occured.')       
+        return result     
 
     def showMainMenu(self):
         if str(self.user["role"][0]).casefold()==str("Admin").casefold():
-            self.moduleValue(1,1,input("1. User Module \n2. Inventory \n3. Purchase Module \n4. Sales Module \nEnter the Value :"))            
+            val=input("\t1. User Module \n\t2. Inventory \n\t3. Purchase Module \n\t4. Sales Module \n\nEnter the Value :")
+            self.moduleValue(val)            
         else:
             if str(self.user["department_name"][0]).casefold()==str("HR").casefold():
-                #self.moduleValue(2,1,input("1. User Module\nEnter the Value :"))
-                
+                self.callUserModule()                
             elif str(self.user["department_name"][0]).casefold()==str("Inventory").casefold():
-                #self.moduleValue(2,2,input("1. Inventory Module\nEnter the Value :")) 
-                InventoryModule(self.user)
+                self.callInventoryModule()
             elif str(self.user["department_name"][0]).casefold()==str("Purchase").casefold():
-                self.moduleValue(2,3,input("1. purhase Module \n2. Inventory Module  \nEnter the Value :"))         
+                user_input=self.checkIntValue(input("\t1. Purhase Module \n\t2. Inventory Module  \nEnter the Value :"))
+                if user_input ==1:
+                    self.callPurchaseModule()
+                elif user_input ==2 :
+                    self.callInventoryModule()
+                else :
+                    print("\tPlease enter a valid input!\n")
+                    self.showMainMenu()
             elif str(self.user["department_name"][0]).casefold()==str("Sales").casefold():
-                self.moduleValue(2,4,input("1. Sales Module \n2. Inventory Module \nEnter the Value :"))         
+                user_input = self.checkIntValue(input("\t1. Sales Module \n\t2. Inventory Module \nEnter the Value :"))                 
+                if user_input==1:
+                    self.callPurchaseModule()
+                elif user_input==2 :
+                    self.callInventoryModule()
+                else:
+                    print("\tPlease enter a valid input!\n")
+                    self.showMainMenu()
+
+    def moduleValue(self,user_input):
+        inp_value = self.checkIntValue(user_input) 
+        if inp_value == 1:
+            self.callUserModule()
+        elif inp_value==2:
+            self.callInventoryModule()
+        elif inp_value==3:
+            self.callPurchaseModule()
+        elif inp_value==4:
+            self.callSalesModule()
+        else:
+            print("\tPlease enter a valid input!\n")
+            self.showMainMenu()
+        
+    def checkIntValue(self,user_input):
+        try:
+            val = int(user_input)
+            return val
+        except ValueError:
+            print("\tInput value is not an Integer. Please enter Integer value alone!\n")
+            self.showMainMenu()  
             
-    def moduleValue(self,method_id,dep_type,inp_value):
-        if method_id==1:
-            if inp_value==1
-                UserModule(self.user)
-            elif
-                InventoryModule(self.user)
+    def callUserModule(self):
+        try:
+            us_mod=userMod.UserModule()
+            us_mod.initCall(self.user)
+        except :
+            print("\tNo file named UserModule is found!\n")
+            self.showMainMenu()
 
-         print("cameeeee",method_id,dep_type,inp_value)
-
-    def callUserModule():
-        UserModule(self.user)
-    
-    def callInventoryModule():
-        InventoryModule(self.user)
-
-    def callPurchaseModule():
-        PurchaseModule(self.user) 
-
-    def callSalesModule():
-        SalesModule(self.user) 
-
+    def callInventoryModule(self):
+        try:
+            inv_mod=inventoryMod.InventoryModule()
+            inv_mod.initCall(self.user)
+        except :
+            print("\tNo file named InventoryModule is found!\n")
+            self.showMainMenu()
+            
+    def callPurchaseModule(self):
+        try:
+            pur_mod=purchaseMod.PurchaseModule()
+            pur_mod.initCall(self.user)
+        except :
+            print("\tNo file named PurchaseModule is found!\n")
+            self.showMainMenu()
+            
+    def callSalesModule(self):
+        try:
+            sales_mod=salesMod.SalesModule()
+            sales_mod.initCall(self.user)
+        except :
+            print("\tNo file named SalesModule is found!\n")
+            self.showMainMenu()
+            
 login_check = Login()        
 login_check.initialCall()
           
